3ef17b89e776e3ab128921d323ba65c1ca8c3cfd
Ingrid Santos <ingrid.dossantos@emailage.com>
2021-06-18 12:09:35 +0100

    fork process

--- fork.sh
+++ fork.sh
@@ -1,21 +1,20 @@
 #/bin/sh
-p1(){
-    echo "> process 1 that will take $1 seconds to complete"
+
+function process {
+    echo "$2 started"
+    start=`date +%s`
+    
+    # Mock process time
     sleep $1
-    echo "> finished p1"
-    exit $2
+
+    end=`date +%s`
+    echo "$2 took $((end-start)) seconds to complete"
+    exit $3
 }
 
-p2(){
-    echo ">> process 2 that will take $1 seconds to complete"
-    sleep $1
-    echo ">> finished p2"
-    exit $2
-}
-
-p1 5 0 &
+process 5 "p1" 0 &
 process_id_1=$!
-p2 2 1 &
+process 2 "p2" 1 &
 process_id_2=$!
 
 wait $process_id_1
@@ -23,4 +22,3 @@ echo Job 1 exited with status $?
 
 wait $process_id_2
 echo Job 2 exited with status $?
-
